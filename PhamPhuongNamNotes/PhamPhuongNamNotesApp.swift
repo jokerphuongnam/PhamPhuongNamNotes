7d1b2535a20f46b820c5794658018c2c72092903
Phạm P. Nam <nampp@inspirelab.io>
2024-01-19 20:58:50 +0700

    Initial Commit

---
+++ PhamPhuongNamNotes/PhamPhuongNamNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhamPhuongNamNotesApp.swift
+//  PhamPhuongNamNotes
+//
+//  Created by P. Nam on 19/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct PhamPhuongNamNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
